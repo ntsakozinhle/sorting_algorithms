af1956d69aabc298fa2ed9c58d97bf1fae7921d5
ntsakozinhle <ntsakosambo1@gmail.com>
2024-02-17 11:15:21 -0800

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+
+/**
+ * lomuto_partition- Lomuto partition scheme for quick sort
+ * @array: the array to be sorted
+ * @low: the strating index of the partition
+ * @high: the ending index of the partion
+ * @size: the size of the array
+ * Return: The pivot indxe
+ */
+
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low -1;
+	int j, tmp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+		}
+	}
+
+	tmp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = tmp;
+	print_array(array, size);
+
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursive function to perform quick sort
+ * @array: The array to be sorted
+ * @low: the starting index of the partion
+ * @high: the ending index of the partion
+ * @size: The size of the array
+ * Return: void
+ */
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using the Quick
+ * sort algorithm (Lomuto partition scheme)
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_recursive(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -34,4 +34,8 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 
 
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+
+
 #endif
